dbe1422aa3897a8c59be01136310b2580baad755
Luis Torres Cereño <torrescereno@gmail.com>
2022-07-17 17:46:12 -0400

    change in link order footer section

--- components/footer.js
+++ components/footer.js
@@ -3,13 +3,13 @@ export default function Footer (){
         <>
             <footer className="relative w-full overflow-hidden bg-gradient-to-r bg-purple-900 text-white">
                 <div className="block px-6 py-10 mx-auto max-w-7xl">
-                    <p className="mb-4 font-semibold text-center text-slate-900">
-                        Idea original de <a className="hover:underline" target="_blank" rel="noopener noreferrer" href="https://twitter.com/diegotherium" >@diegotherium</a>
-                    </p>
                     <p className="font-semibold text-center text-slate-600">
                         <a className="hover:underline" href="/pdf/tabla.pdf" target="_blank" rel="noopener noreferrer">
                             Descarga PDF de la tabla periódica
                         </a>
+                    </p>
+                    <p className="mb-4 font-semibold text-center text-slate-900">
+                        Idea original de <a className="hover:underline" target="_blank" rel="noopener noreferrer" href="https://twitter.com/diegotherium" >@diegotherium</a>
                     </p>
                 </div>
             </footer>
